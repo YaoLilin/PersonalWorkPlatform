5af99d77807deefe4788cbd42c06dc73d0fcf965
yaolilin <yaolilin@outlook.com>
2024-10-21 10:21:13 +0800

    测试类修改

---
+++ work-platform-server/src/test/java/com/personalwork/base/TestSetUp.java
@@ -0,0 +1,35 @@
+package com.personalwork.base;
+
+import com.personalwork.modal.entity.UserDo;
+import com.personalwork.security.bean.UserDetail;
+import com.personalwork.util.UserUtil;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.when;
+
+/**
+ * @author yaolilin
+ * @desc 测试类通用的初始化
+ * @date 2024/10/17
+ **/
+public class TestSetUp {
+
+    @BeforeEach
+    public void before(){
+        MockitoAnnotations.openMocks(this);
+        Mockito.mockStatic(UserUtil.class);
+        when(UserUtil.getLoginUserId()).thenReturn(1);
+        UserDo userDo = new UserDo();
+        userDo.setId(1);
+        UserDetail userDetail = new UserDetail(userDo);
+        when(UserUtil.getLoginUser()).thenReturn(userDetail);
+    }
+
+    @AfterEach
+    public void after(){
+        Mockito.framework().clearInlineMocks();
+    }
+}
